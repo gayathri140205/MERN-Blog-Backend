3a34a15475e7eae355fa06034d1a66957eb66fbf
gayathri140205 <gayathri140205@gmail>
2024-11-10 22:58:36 +0530

    API

--- Server.js
+++ Server.js
@@ -18,9 +18,17 @@ app.use('/api/auth', authRoutes);
 app.use('/api/blogs', blogRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI,)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  connectTimeoutMS: 30000,  // 30 seconds timeout
+})
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+  });
 
 // Start server
 const PORT = process.env.PORT || 5000;
